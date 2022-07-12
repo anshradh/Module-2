eaf7fb07e8ca86828f07dd2a8051cfa4cdb50bf7
Ansh Radhakrishnan <anshuman.radhakrishnan@yale.edu>
2022-07-12 07:35:43 -0700

    2.5

--- project/run_tensor.py
+++ project/run_tensor.py
@@ -21,8 +21,9 @@ class Network(minitorch.Module):
         self.layer3 = Linear(hidden_layers, 1)
 
     def forward(self, x):
-        # TODO: Implement for Task 2.5.
-        raise NotImplementedError('Need to implement for Task 2.5')
+        a1 = self.layer1.forward(x).relu()
+        a2 = self.layer2.forward(a1).relu()
+        return self.layer3.forward(a2).sigmoid()
 
 
 class Linear(minitorch.Module):
@@ -33,8 +34,10 @@ class Linear(minitorch.Module):
         self.out_size = out_size
 
     def forward(self, x):
-        # TODO: Implement for Task 2.5.
-        raise NotImplementedError('Need to implement for Task 2.5')
+        b, in_size = x.shape
+        return (
+            self.weights.value.view(1, in_size, self.out_size) * x.view(b, in_size, 1)
+        ).sum(1).view(b, self.out_size) + self.bias.value.view(self.out_size)
 
 
 def default_log_fn(epoch, total_loss, correct, losses):
